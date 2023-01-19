2eaf1a4dc0cc4bc47430a6f47dbb898a28214372
Camille Linan <camille.linan@hotmail.com>
2023-01-19 23:22:17 +0100

    add section date

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,17 +1,15 @@
-import template1 from '../../styles/img/template1.png'
-import template2 from '../../styles/img/template2.png'
+import template3 from '../../styles/img/template3.png'
 
 const HomePage = () => {
     return(
         <section className='homepage'>
             <article className='homepage_date'>
-                <img src={template1} alt='feuille en aquarelle' className='homepage_date_img homepage_date_img_1' />
+                <img src={template3} alt='feuille en aquarelle' className='homepage_date_img homepage_date_img_1' />
                 <div className='homepage_date_container'>
                     <h3>Nous nous marions !</h3>
                     <h3>LE 21 JUIN 2024</h3>
                     <h3>À Paris</h3>
                 </div>
-                <img src={template2} alt='feuille en aquarelle' className='homepage_date_img homepage_date_img_2' />
             </article>
         </section>
     )
