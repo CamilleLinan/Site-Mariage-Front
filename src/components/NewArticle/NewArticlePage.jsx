03b5a14f87f389692a774553c0ed8e3b28d373b3
Camille Linan <camille.linan@hotmail.com>
2023-01-22 23:47:25 +0100

    new article template OK

--- src/components/NewArticle/NewArticlePage.jsx
+++ src/components/NewArticle/NewArticlePage.jsx
@@ -1,52 +1,58 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom"
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const NewArticlePage = () => {
+    const [description, setDescription] = useState('');
+
     return (
-        //backdrop dans section
-        <section className="new_article"> 
-            <div className="new_article_container">
-                <h2 className="new_article_container_title">Ajouter un nouvel article</h2>
-                <form action="" id='' className="new_article_container_form"> 
-                    <div className="new_article_container_form_section">
-                        <label htmlFor="title" className="new_article_container_form_section">Titre de l'article :</label>
-                        <br/>
-                        <input 
-                            type='text'
-                            name="title"
-                            id="title"
-                            className="new_article_container_form_input"
-                            required
-                        /> 
-                        <br />
+        <section className="new_article">
+            <header className="new_article_header">
+                <h2 className="new_article_header_title">Ajouter un nouvel article</h2>
+            </header> 
+            
+            <form action="" id='' className="new_article_form"> 
+                <div className="new_article_form_section">
+                    <label htmlFor="title" className="new_article_form_label">Titre de l'article :</label>
+                    <br/>
+                    <input 
+                        type='text'
+                        name="title"
+                        id="title"
+                        className="new_article_form_input new_article_form_input_title"
+                        required
+                    /> 
+                    <br />
 
-                        <label htmlFor="description" className="new_article_container_form_section">Description :</label>
-                        <br/>
-                        <textarea
-                            type='text'
-                            name="description"
-                            id="description"
-                            className="form_input update_infos_input"
-                            required
-                        /> 
-                        <br />
+                    <label htmlFor="description" className="new_article_form_label">Description :</label>
+                    <br/>
+                    <ReactQuill
+                        theme="snow"
+                        name='description' 
+                        id="description"
+                        value={description} 
+                        onChange={setDescription} 
+                        className="new_article_form_input new_article_form_input_description"
+                        required 
+                    />
 
-                        <label htmlFor="file" className="new_article_container_form_section">Ajouter une image :</label>
-                        <br/>
-                        <input 
-                            type="file" 
-                            name="file" 
-                            id="file"
-                            className="form_input update_infos_input"
-                            accept=".jpg, .jpeg, .png, .gif"
-                        />
-                    </div>
-                    
-                    <footer className="new_article_footer">
-                        <NavLink className="new_article_footer_btn btn_cancel" title="Annuler" end to='/blog'>Annuler</NavLink>
-                        <NavLink className="new_article_footer_btn btn_confirm">Confirmer</NavLink>
-                    </footer>
-                </form>
-            </div>
+                    <label htmlFor="file" className="new_article_form_label">Ajouter une image :</label>
+                    <br/>
+                    <input 
+                        type="file" 
+                        name="file" 
+                        id="file"
+                        className=""
+                        accept=".jpg, .jpeg, .png, .gif"
+                    />
+                </div>
+                
+                <footer className="new_article_footer">
+                    <NavLink className="new_article_footer_btn new_article_footer_btn_cancel" title="Annuler" end to='/blog'>Annuler</NavLink>
+                    <NavLink className="new_article_footer_btn new_article_footer_btn_confirm">Confirmer</NavLink>
+                </footer>
+            </form>
         </section> 
     )
 }
