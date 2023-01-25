5af3578d97936eb3c5a5356e79dd185723df20dd
Camille Linan <camille.linan@hotmail.com>
2023-01-26 00:51:57 +0100

    add comment template

--- src/components/Blog/BlogArticle.jsx
+++ src/components/Blog/BlogArticle.jsx
@@ -1,3 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
+
+const commentIcon = <FontAwesomeIcon icon={faComment} />
+
 const BlogArticle = (props) => {
     const { img, title, description } = props;
     
@@ -7,6 +12,7 @@ const BlogArticle = (props) => {
             <h3 className="blog_article_title">{title}<span className="blog_article_title_underline"></span></h3>
             {img && <img src={img} alt='blog-mariage-article-cover' className='blog_article_img' />}
             <p className="blog_article_content">{description}</p>
+            <p className='blog_article_comment'>0 {commentIcon} - Laisser un commentaire</p>
         </article>
     )
 }
