bc4df2345caa0c29283be5c6073610b7f272a0cb
Camille Linan <camille.linan@hotmail.com>
2023-01-25 22:26:31 +0100

    new article form OK

---
+++ src/components/NewArticle/NewArticleForm.jsx
@@ -0,0 +1,107 @@
+import { useContext, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom"
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import AuthContext from "../../context/authContext";
+import axios from "axios";
+
+const NewArticleForm = () => {
+    const authCtx = useContext(AuthContext);
+
+    const [ articleTitle, setArticleTitle ] = useState('')
+    const [ articleDescription, setArticleDescription ] = useState('');
+    const [ previewPicture, setPreviewPicture ] = useState('');
+    const [ articlePicture, setArticlePicture ] = useState('');
+
+    const [ errorServer, setErrorServer ] = useState('');
+
+    const changeHandlerPicture = (e) => {
+        let newPicture;
+
+        if (e.target.files) {
+            newPicture = URL.createObjectURL(e.target.files[0])
+            setPreviewPicture(newPicture)
+        }
+        setArticlePicture(e.target.files[0]) 
+    }
+
+    const url = `http://localhost:5000/api/articles`
+    const navigate = useNavigate()
+    
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        let formData = new FormData();
+        formData.append('userId', authCtx.userId);
+        formData.append('title', articleTitle);
+        formData.append('description', articleDescription);
+        formData.append('image', articlePicture);
+
+        await axios.post(url, formData, {
+            headers: {
+                Authorization: `Bearer ${authCtx.token}`,
+                'Content-Type': `multipart/form-data`,
+            },
+        })
+            .then(() => {
+                alert('Votre article va être publié !');
+                navigate('/blog');
+            })
+            .catch(() => {
+                setErrorServer({ ...errorServer, message: 'Une erreur interne est survenue. Merci de revenir plus tard.' })      
+            });
+    };
+
+    return(
+        <form action="" onSubmit={onSubmit} id='new-article-form' className="new_article_form"> 
+            <div className="new_article_form_section">
+                <label htmlFor="title" className="new_article_form_label">Titre de l'article :</label>
+                    <input 
+                        type='text'
+                        name="title"
+                        id="title"
+                        className="new_article_form_input new_article_form_input_title"
+                        value={articleTitle}
+                        onChange={(e) => setArticleTitle(e.target.value)}
+                        required
+                    /> 
+                    <br />
+
+                <label htmlFor="description" className="new_article_form_label">Description :</label>
+                    <ReactQuill
+                        theme="snow"
+                        name='description' 
+                        id="description"
+                        className="new_article_form_input new_article_form_input_description"
+                        value={articleDescription} 
+                        onChange={(e) => setArticleDescription(e.target.value)} 
+                        required 
+                    />
+                
+                <label htmlFor="file" className="new_article_form_label">Ajouter une image :</label>
+                    <div className="new_article_form_container">
+                        {previewPicture && <img src={previewPicture} alt='article-mariage-cover' className="new_article_form_img" />}
+                        <input 
+                            type="file" 
+                            name="file" 
+                            id="file"
+                            className=""
+                            accept=".jpg, .jpeg, .png, .gif"
+                            onChange={changeHandlerPicture}
+                        />
+                    </div>
+            </div>
+            
+            <footer className="new_article_footer">
+                <NavLink className="new_article_footer_btn new_article_footer_btn_cancel" title="Annuler" end to='/blog'>
+                    Annuler
+                </NavLink>
+                <button type="submit" className="new_article_footer_btn new_article_footer_btn_confirm">
+                    Confirmer
+                </button>
+            </footer>
+        </form>
+    )
+}
+
+export default NewArticleForm
--- src/pages/NewArticle.jsx
+++ src/pages/NewArticle.jsx
@@ -1,15 +1,41 @@
+import axios from "axios"
+import { useCallback, useContext, useEffect, useState } from "react"
 import BannerBottomPages from "../components/Layout/BannerBottomPages"
 import BannerTopPages from "../components/Layout/BannerTopPages"
 import Footer from "../components/Layout/Footer"
 import Header from "../components/Layout/Header"
 import NewArticlePage from "../components/NewArticle/NewArticlePage"
+import AuthContext from "../context/authContext"
 
 const NewArticle = () => {
+    const [ userData, setUserData ] = useState('');
+    const [ errorServer, setErrorServer ] = useState('');
+
+    const authCtx = useContext(AuthContext);
+
+    const getUserData = useCallback(async () => {
+        await axios({
+            method: 'GET',
+            url: `http://localhost:5000/api/users/${authCtx.userId}`,
+            headers: {
+                Authorization: `Bearer ${authCtx.token}`,
+            }
+        })
+            .then((res) => { setUserData(res.data) })
+            .catch(() => {
+                setErrorServer({ ...errorServer, message: 'Une erreur interne est survenue. Merci de revenir plus tard.' });
+            });
+    },[authCtx.token, authCtx.userId, errorServer]);
+
+    useEffect(() => {
+        getUserData();
+    }, [getUserData])
+
     return (
         <>
             <Header />
             <BannerTopPages />
-            <NewArticlePage />
+            <NewArticlePage propIsAdmin={userData.isAdmin} />
             <BannerBottomPages />
             <Footer />
         </>
