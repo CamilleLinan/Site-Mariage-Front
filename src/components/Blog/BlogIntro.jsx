25e1d8031de70f25b7e30ec3e99907d8f6bc7388
Camille Linan <camille.linan@hotmail.com>
2023-01-30 00:56:08 +0100

    minor corrections

--- src/components/Blog/BlogIntro.jsx
+++ src/components/Blog/BlogIntro.jsx
@@ -8,17 +8,18 @@ const BlogIntro = ({ propIsAdmin }) => {
     return (
         <article className="blog_intro">
             <h2 className="blog_intro_title">Blog du mariage</h2>
-            
-            <p>Bienvenue sur notre blog de mariage !</p> 
-            <p>Nous sommes tellement heureux de partager notre histoire d'amour 
-            avec vous tous et de vous accompagner dans les préparatifs de notre 
-            grand jour.</p>
-            <p>Depuis notre rencontre il y a quelques années, nous avons rêvé de 
-            ce moment où nous célébrerions notre amour devant nos proches et nos 
-            amis. Et maintenant, le moment est enfin arrivé !</p>
-            <p>Nous espérons que notre blog vous inspirera et vous aidera dans la 
-            planification de votre propre mariage.</p>
-            
+            <div className='blog_intro_content'>
+                <p>Bienvenue sur notre blog de mariage !</p> 
+                <p>Nous sommes tellement heureux de partager notre histoire d'amour 
+                avec vous tous et de vous accompagner dans les préparatifs de notre 
+                grand jour.</p>
+                <p>Depuis notre rencontre il y a quelques années, nous avons rêvé de 
+                ce moment où nous célébrerions notre amour devant nos proches et nos 
+                amis. Et maintenant, le moment est enfin arrivé !</p>
+                <p>Nous espérons que notre blog vous inspirera et vous aidera dans la 
+                planification de votre propre mariage.</p>
+            </div>
+
             {propIsAdmin === true &&
                 <NavLink className="blog_intro_link" title='Nouvel article' end to='/newArticle'>
                     <i className="blog_intro_link_icon">{plusIcon}</i> 
--- src/components/Confirm/ConfirmPageIntro.jsx
+++ src/components/Confirm/ConfirmPageIntro.jsx
@@ -2,10 +2,12 @@ const ConfirmPageIntro = () => {
     return(
         <>
             <h2 className="confirm_section_title">Confirmer sa présence</h2>
-            <p className="confirm_section_content">Pour confirmer votre présence à notre mariage, vous 
-            pouvez écrire votre nom et cliquer sur Rechercher.</p>
-            <p className="confirm_section_content">Vous verrez alors apparaître votre nom et pourrez confirmer ou 
-            non votre venue.</p>
+            <div className='blog_intro_content'>
+                <p className="confirm_section_content">Pour confirmer votre présence à notre mariage, vous 
+                pouvez écrire votre nom et cliquer sur Rechercher.</p>
+                <p className="confirm_section_content">Vous verrez alors apparaître votre nom et pourrez confirmer ou 
+                non votre venue.</p>
+            </div>
         </>
     )
 }
